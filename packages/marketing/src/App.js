e468e3f89864d3ef12f1cfbd0438f36463dd2959
PERFICIENT\vikram.rambhad <vikram.rambhad@perficient.com>
2026-01-21 13:52:19 +0530

    added navigation and fix the navigation errors

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,23 +1,23 @@
 import React from 'react'
-import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import { Switch, Route, Router } from 'react-router-dom'
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
 
 const generateClassName = createGenerateClassName({
-    productionPrefix: 'ma'
+    productionPrefix: 'MA'
 })
-const App = () => {
+const App = ({ history }) => {
     return (
         <div>
             <StylesProvider generateClassName={generateClassName}>
-                <BrowserRouter>
+                <Router history={history}>
                     <Switch>
                         <Route exact path="/pricing" component={Pricing} />
                         <Route path='/' component={Landing} />
                     </Switch>
-                </BrowserRouter>
+                </Router>
             </StylesProvider>
         </div>
     )
